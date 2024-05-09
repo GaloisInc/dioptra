2fb98c3d1beac041e05afc514252614c18be25c8
Chris Phifer <cphifer@galois.com>
2024-05-09 02:08:22 -0700

    shell.nix: Provide a Nix shell for the repository.
    This commit provides a shell.nix at the root of the repository, so
    users/developers can easily enter an environment suitable to run
    our tool and other OpenFHE applications.
    
    In particular, the shell provides Python 3 with the OpenFHE
    bindings, and nothing more. Additional packages can be loaded with
    Nix, and this may be the case as we develop Dioptra and determine
    what will be required for deployment (ideally, the Nix shell will
    be set up to allow Dioptra to be immediately used on a script using
    OpenFHE). That is: This expression is subject to change!

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+
+  openfhe-development = pkgs.callPackage ./nix/openfhe-development.nix { };
+  openfhe = pkgs.python3Packages.callPackage ./nix/openfhe-python.nix {
+    openfhe-development = openfhe-development;
+  };
+in
+
+pkgs.mkShellNoCC {
+  packages = [
+    (pkgs.python3.withPackages (_: [ openfhe ]))
+  ];
+}
