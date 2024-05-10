413e2e7da8c2d43de8aed24548baec8b842905f9
Chris Phifer <cphifer@galois.com>
2024-05-10 01:28:53 -0700

    dioptra: Use a more idiomatic __init__.py, move CLI to its own module.

---
+++ src/dioptra/cli.py
@@ -0,0 +1,42 @@
+import click
+import click_log
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
+
+
+@click.group()
+@click_log.simple_verbosity_option(logger)
+def cli() -> None:
+    """The Dioptra FHE platform."""
+    pass
+
+
+@cli.command()
+@click.argument("script", type=click.Path(exists=True))
+def run(script: click.Path) -> None:
+    """Run an OpenFHE SCRIPT normally."""
+    click.echo("Running OpenFHE program...")
+
+
+@cli.group()
+def estimate() -> None:
+    """Estimate properties of an OpenFHE program."""
+    pass
+
+
+@estimate.command()
+@click.argument("script", type=click.File("r"))
+def peakmem(script: click.File) -> None:
+    """Estimate peak memory usage of an OpenFHE SCRIPT."""
+    click.echo("Estimating memory...")
+
+
+@estimate.command()
+@click.argument("script", type=click.File("r"))
+def runtime(script: click.File) -> None:
+    """Estimate the runtime of an OpenFHE SCRIPT."""
+    click.echo("Estimating runtime...")
