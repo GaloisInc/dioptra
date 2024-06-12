fb49f69c52b3a731c2bb071ea363cc6c2d85e429
Chris Phifer <cphifer@galois.com>
2024-06-12 10:56:02 -0700

    cli: Update the type of the script CLI command parameters.
    Previously, the types of the 'script' parameters to dioptra commands
    were of incorrect types. Furthermore, the estimation commands utilized
    click.File to intelligently handle the opening and closing of files.
    
    This commit updates the type of the 'script' parameters to
    pathlib.Path, and consistently uses click.Path as the argument type,
    deferring the handling of files to later.

--- src/dioptra/cli.py
+++ src/dioptra/cli.py
@@ -2,6 +2,7 @@ import click
 import click_log
 
 import logging
+from pathlib import Path
 
 
 logger = logging.getLogger(__name__)
@@ -17,7 +18,7 @@ def cli() -> None:
 
 @cli.command()
 @click.argument("script", type=click.Path(exists=True))
-def run(script: click.Path) -> None:
+def run(script: Path) -> None:
     """Run an OpenFHE SCRIPT normally."""
     click.echo("Running OpenFHE program...")
 
@@ -29,14 +30,14 @@ def estimate() -> None:
 
 
 @estimate.command()
-@click.argument("script", type=click.File("r"))
-def peakmem(script: click.File) -> None:
+@click.argument("script", type=click.Path(exists=True))
+def peakmem(script: Path) -> None:
     """Estimate peak memory usage of an OpenFHE SCRIPT."""
     click.echo("Estimating memory...")
 
 
 @estimate.command()
-@click.argument("script", type=click.File("r"))
-def runtime(script: click.File) -> None:
+@click.argument("script", type=click.Path(exists=True))
+def runtime(script: Path) -> None:
     """Estimate the runtime of an OpenFHE SCRIPT."""
     click.echo("Estimating runtime...")
