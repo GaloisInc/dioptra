8905be2bdf7dcdd0ca021ca518ba06fbc85b81f3
Rawane Issa <rissa@galois.com>
2024-07-29 17:03:40 -0700

    Add more operations and factor code
    Remove unecessary instruction id and store analysis based on the unique id
    of the destination ciphertext. Add operations for Op(Ciphertext, Plaintext).

--- src/dioptra/analyzer/metrics/runtime.py
+++ src/dioptra/analyzer/metrics/runtime.py
@@ -1,4 +1,4 @@
-from dioptra.analyzer.metrics.analysisbase import AnalysisBase, Ciphertext
+from dioptra.analyzer.metrics.analysisbase import AnalysisBase, Ciphertext, Plaintext
 from dioptra.analyzer.metrics.multdepth import MultDepth
 from inspect import Traceback
 import dis
@@ -13,7 +13,6 @@ class Runtime(AnalysisBase):
 
     def __init__(self, multiplicative_depth: MultDepth, runtime_table: dict[str, int]) -> None:
         self.total_runtime = 0
-        self.instruction_num = 0
         self.multiplicative_depth = multiplicative_depth
         self.runtime_table = runtime_table
         self.where = {}
@@ -21,29 +20,40 @@ class Runtime(AnalysisBase):
         
 
     def trace_mul_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Traceback) -> None:
-        self.instruction_num += 1
-        (mult_depth, _, _) = self.multiplicative_depth.where[self.instruction_num]
+        (mult_depth, _, _) = self.multiplicative_depth.where[dest]
         self.total_runtime += self.runtime_table[("mult_ctct", mult_depth)]
         self.set_runtime(dest, self.total_runtime, call_loc)
-    
+
+    def trace_mul_ctpt(self, dest: Ciphertext, ct1: Ciphertext, ct2: Plaintext, call_loc: Traceback) -> None:
+        (mult_depth, _, _) = self.multiplicative_depth.where[dest]
+        self.total_runtime += self.runtime_table[("mult_ctpt", mult_depth)]
+        self.set_runtime(dest, self.total_runtime, call_loc)
+
     def trace_add_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Traceback) -> None:
-        self.instruction_num += 1
-        (mult_depth, _, _) = self.multiplicative_depth.where[self.instruction_num]
+        (mult_depth, _, _) = self.multiplicative_depth.where[dest]
         self.total_runtime += self.runtime_table[("add_ctct", mult_depth)]
         self.set_runtime(dest, self.total_runtime, call_loc)
 
+    def trace_add_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Plaintext, call_loc: Traceback) -> None:
+        (mult_depth, _, _) = self.multiplicative_depth.where[dest]
+        self.total_runtime += self.runtime_table[("add_ctpt", mult_depth)]
+        self.set_runtime(dest, self.total_runtime, call_loc)
+
     def trace_sub_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Traceback) -> None:
-        self.instruction_num += 1
-        (mult_depth, _, _) = self.multiplicative_depth.where[self.instruction_num]
+        (mult_depth, _, _) = self.multiplicative_depth.where[dest]
         self.total_runtime += self.runtime_table[("sub_ctct", mult_depth)]
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def trace_bootstrap(self, dest: Ciphertext, ct1: Ciphertext, call_loc: Traceback | None) -> None:
-        self.instruction_num += 1
-        (mult_depth, _, _) = self.multiplicative_depth.where[self.instruction_num]
-        self.total_runtime += self.runtime_table[("bootstrap", -1)]
+    def trace_sub_ctpt(self, dest: Ciphertext, ct1: Ciphertext, ct2: Plaintext, call_loc: Traceback) -> None:
+        (mult_depth, _, _) = self.multiplicative_depth.where[dest]
+        self.total_runtime += self.runtime_table[("sub_ctpt", mult_depth)]
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def set_runtime(self, _ct: Ciphertext, runtime: int, call_loc: Traceback) -> None:
-        self.where[self.instruction_num] = (runtime, call_loc.filename, call_loc.positions) # type: ignore
+    def trace_bootstrap(self, dest: Ciphertext, ct1: Ciphertext, call_loc: Traceback | None) -> None:
+        (mult_depth, _, _) = self.multiplicative_depth.where[dest]
+        self.total_runtime += self.runtime_table[("bootstrap", mult_depth)]
+        self.set_runtime(dest, self.total_runtime, call_loc)
+
+    def set_runtime(self, ct: Ciphertext, runtime: int, call_loc: Traceback) -> None:
+        self.where[ct] = (runtime, call_loc.filename, call_loc.positions) # type: ignore
     
