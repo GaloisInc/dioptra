6938815916924d6a6e2b0a83dd252809ce0f12db
James LaMar <james.lamar@galois.com>
2024-08-14 16:22:26 -0700

    Add `simple_features_example.py` as an initial step toward demoablity
    Misc associated cleanup and fixes

--- src/dioptra/analyzer/calibration.py
+++ src/dioptra/analyzer/calibration.py
@@ -7,29 +7,7 @@ import enum
 import json
 import sys
 
-def format_ns(ns: int) -> str:
-  micro = ns // 1000
-  milli = micro // 1000
-  second = milli // 1000
-
-  tstr = f"{ns % 1000:03}n"
-  if micro > 0:
-    tstr = f"{micro%1000:03}u" + tstr
-  if milli > 0:
-    tstr = f"{milli%1000:03}m" + tstr
-  if second > 0:
-    tstr = f"{second}s" + tstr
-
-  return tstr
-
-# range function with step that always includes endpoints
-def ep_range(start: int, end: int, step: int) -> Iterator[int]:
-  i = start
-  while i < end - 1:
-    yield i
-    i += step
-
-  yield end - 1
+from dioptra.analyzer.utils.util import format_ns
 
 
 class EventKind(enum.Enum):
@@ -71,7 +49,7 @@ class RuntimeTable:
       e_swaped = Event(e.kind, e.arg_depth2, e.arg_depth1)
       return self.runtimes[e_swaped]
     else:
-      raise(f"No runtime found for event: kind:{e.kind} depth 1:{e.arg_depth1} depth 2:{e.arg_depth2}")
+      raise NotImplementedError(f"No runtime found for event: kind:{e.kind} depth 1:{e.arg_depth1} depth 2:{e.arg_depth2}")
 
 class RuntimeSamples:
   def __init__(self):
@@ -150,13 +128,16 @@ class RuntimeSample:
 
 class Calibration:
   def __init__(self, 
+    cc: openfhe.CryptoContext,
     params: openfhe.CCParamsBFVRNS | openfhe.CCParamsBGVRNS | openfhe.CCParamsCKKSRNS, 
     out: TextIO | None = None,
     sample_count: int = 5,
   ) -> None:
+
     self.params = params
     self.out = out
     self.sample_count = sample_count
+    self.cc = cc
 
   def log(self, msg: str) -> None:
     if self.out is not None:
@@ -185,20 +166,9 @@ class Calibration:
 
       return RuntimeSample(Event(label, a1, a2), samples, on_exit=on_exit)
 
-    cc = openfhe.GenCryptoContext(self.params)
-    cc.Enable(openfhe.PKESchemeFeature.PKE)
-    cc.Enable(openfhe.PKESchemeFeature.KEYSWITCH)
-    cc.Enable(openfhe.PKESchemeFeature.LEVELEDSHE)
-    cc.Enable(openfhe.PKESchemeFeature.ADVANCEDSHE)
-    cc.Enable(openfhe.PKESchemeFeature.FHE)
+    cc = self.cc
 
     self.log("Generating evaluation keys...")
-
-    # TODO: make these configurable
-    level_budget = [3, 3]
-    bsgs_dim = [0,0]
-    cc.EvalBootstrapSetup(level_budget, bsgs_dim, self.num_slots())
-
     # key generation
     key_pair = cc.KeyGen()
     cc.EvalMultKeyGen(key_pair.secretKey)
@@ -284,99 +254,3 @@ def run_example():
   s.read_json("balanced.samples")
   assert s == samples
 
-
-
-
-
-
-# def call_with_time(f, *args, **kwargs):
-#   begin = time.process_time_ns()
-#   result = f(*args, **kwargs)
-#   end = time.process_time_ns()
-#   return (result, end - begin)
-
-
-# def mk_fresh_plaintext(cc: openfhe.CryptoContext) -> openfhe.Plaintext:
-#   x = [random.uniform(0, 1) for i in range(num_slots)]
-#   x_pt = cryptocontext.MakeCKKSPackedPlaintext(x)
-#   x_pt.SetLength(num_slots)
-#   return x_pt
-
-
-# def mk_fresh_ciphertext(cc: openfhe.CryptoContext, key: openfhe.PublicKey) -> openfhe.Ciphertext:
-#   return cc.Encrypt(key, mk_fresh_plaintext(cc))
-
-
-# stats: list[tuple[str, int, int, int]] = []
-# def record(ct: openfhe.Ciphertext, label: str, t: int = 0):
-#     stats.append((label, ct.GetLevel(), ct.GetSlots(), t))
-#     return ct
-
-# def time_ct(desc: str, f, *args, **kwargs) -> openfhe.Ciphertext:
-#    (ct, t) = call_with_time(f, *args, **kwargs)
-#    return record(ct, desc, t)
-
-# # Setup Parameters
-# parameters = openfhe.CCParamsCKKSRNS()
-# secret_key_dist = openfhe.SecretKeyDist.UNIFORM_TERNARY
-# parameters.SetSecretKeyDist(secret_key_dist)
-# parameters.SetSecurityLevel(openfhe.SecurityLevel.HEStd_NotSet)
-# parameters.SetRingDim(1 << 16)
-
-# rescale_tech = openfhe.ScalingTechnique.FLEXIBLEAUTO
-
-# dcrt_bits = 59
-# first_mod = 60
-
-# parameters.SetScalingModSize(dcrt_bits)
-# parameters.SetScalingTechnique(rescale_tech)
-# parameters.SetFirstModSize(first_mod)
-
-# num_iterations = 2
-
-# level_budget = [3, 3]
-# bsgs_dim = [0,0]
-  
-# depth = 10 + openfhe.FHECKKSRNS.GetBootstrapDepth(level_budget, secret_key_dist) + (num_iterations - 1)
-
-# parameters.SetMultiplicativeDepth(depth)
-
-# # Make context and enable features
-
-# cryptocontext = openfhe.GenCryptoContext(parameters)
-
-# cryptocontext.Enable(openfhe.PKESchemeFeature.PKE)
-# cryptocontext.Enable(openfhe.PKESchemeFeature.KEYSWITCH)
-# cryptocontext.Enable(openfhe.PKESchemeFeature.LEVELEDSHE)
-# cryptocontext.Enable(openfhe.PKESchemeFeature.ADVANCEDSHE)
-# cryptocontext.Enable(openfhe.PKESchemeFeature.FHE)
-
-# # precomputation of keys
-
-# num_slots = 2**15
-# cryptocontext.EvalBootstrapSetup(level_budget, bsgs_dim, num_slots)
-
-# # key generation
-# key_pair = cryptocontext.KeyGen()
-# cryptocontext.EvalMultKeyGen(key_pair.secretKey)
-# cryptocontext.EvalBootstrapKeyGen(key_pair.secretKey, num_slots)
-
-
-# stats = []
-
-# ct = time_ct("fresh ct", mk_fresh_ciphertext, cryptocontext, key_pair.publicKey)
-# cts_by_depth = [ct]
-# for i in range(0, depth * 4):
-#   print(i)
-#   ct = time_ct(f"EvalMult: fresh * depth {i}", cryptocontext.EvalMult, ct,  mk_fresh_ciphertext(cryptocontext, key_pair.publicKey))
-#   cts_by_depth.append(ct)
-
-# for i in range(0, depth):
-#   for j in range(i, depth):
-#       time_ct(f"EvalMult: depth {i} * depth {j}", cryptocontext.EvalMult, cts_by_depth[i], cts_by_depth[j])
-
-# for i in range(0, depth):
-#   time_ct(f"EvalBootstrap: {i}", cryptocontext.EvalBootstrap, cts_by_depth[1])
-
-# for (label, levels, slots, t) in stats:
-#   print(f"\"{label}\",{levels},{slots},{format_ns(t)}" )
