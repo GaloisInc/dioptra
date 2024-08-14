a83c7216ee5fa966365f07ca5195fc67ea70bfa4
Rawane Issa <rissa@galois.com>
2024-08-14 02:40:47 +0000

    Propagate runtimes across file

--- src/dioptra/analyzer/metrics/runtime.py
+++ src/dioptra/analyzer/metrics/runtime.py
@@ -1,7 +1,7 @@
 from dioptra.analyzer.metrics.analysisbase import AnalysisBase, Ciphertext, Plaintext, PublicKey, PrivateKey
 from dioptra.analyzer.metrics.multdepth import MultDepth
 from dioptra.analyzer.calibration import RuntimeTable, RuntimeSamples, Event, EventKind
-from inspect import Traceback
+from dioptra.analyzer.utils.code_loc import Frame
 import dis
 
 class Runtime(AnalysisBase):
@@ -14,49 +14,51 @@ class Runtime(AnalysisBase):
         self.where = {}
         self.unit = "nanosec"
 
-    def trace_encode(self, dest: Plaintext, level: int, call_loc: Traceback) -> None:
+    def trace_encode(self, dest: Plaintext, level: int, call_loc: Frame) -> None:
         dest_depth = level
         self.total_runtime += self.runtime_table.get_runtime_ns(Event(EventKind.ENCODE, dest_depth))
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def trace_encode_ckks(self, dest: Plaintext, value: list[float], call_loc: Traceback, level: int = 0)  -> None:
+    def trace_encode_ckks(self, dest: Plaintext, value: list[float], call_loc: Frame, level: int = 0)  -> None:
         dest_depth = level
         self.total_runtime += self.runtime_table.get_runtime_ns(Event(EventKind.ENCODE, dest_depth))
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def trace_encrypt(self, dest: Ciphertext, publicKey: PublicKey, plaintext: Plaintext, call_loc: Traceback) -> None:
+    def trace_encrypt(self, dest: Ciphertext, publicKey: PublicKey, plaintext: Plaintext, call_loc: Frame) -> None:
         dest_depth = plaintext.level
         self.total_runtime += self.runtime_table.get_runtime_ns(Event(EventKind.ENCRYPT, dest_depth))
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def trace_decrypt(self, dest: Plaintext, publicKey: PublicKey, ciphertext: Ciphertext, call_loc: Traceback) -> None:
+    def trace_decrypt(self, dest: Plaintext, publicKey: PublicKey, ciphertext: Ciphertext, call_loc: Frame) -> None:
         ct1_depth = self.multiplicative_depth.depth[ciphertext]
         self.total_runtime += self.runtime_table.get_runtime_ns(Event(EventKind.DECRYPT, ct1_depth))
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def trace_mul_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Traceback) -> None:
+    def trace_mul_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Frame) -> None:
         ct1_depth = self.multiplicative_depth.depth[ct1]
         ct2_depth = self.multiplicative_depth.depth[ct2]
         
         self.total_runtime += self.runtime_table.get_runtime_ns(Event(EventKind.EVAL_MULT, ct1_depth, ct2_depth))
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def trace_add_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Traceback) -> None:
+    def trace_add_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Frame) -> None:
         ct1_depth = self.multiplicative_depth.depth[ct1]
         ct2_depth = self.multiplicative_depth.depth[ct2]
         self.total_runtime += self.runtime_table.get_runtime_ns(Event(EventKind.EVAL_ADD, ct1_depth, ct2_depth))
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def trace_sub_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Traceback) -> None:
+    def trace_sub_ctct(self, dest: Ciphertext, ct1: Ciphertext, ct2: Ciphertext, call_loc: Frame) -> None:
         ct1_depth = self.multiplicative_depth.depth[ct1]
         ct2_depth = self.multiplicative_depth.depth[ct2]
         self.total_runtime += self.runtime_table.get_runtime_ns(Event(EventKind.EVAL_SUB, ct1_depth, ct2_depth))
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def trace_bootstrap(self, dest: Ciphertext, ct1: Ciphertext, call_loc: Traceback | None) -> None:
+    def trace_bootstrap(self, dest: Ciphertext, ct1: Ciphertext, call_loc: Frame | None) -> None:
         self.total_runtime += self.runtime_table.get_runtime_ns(Event(EventKind.EVAL_BOOTSTRAP))
         self.set_runtime(dest, self.total_runtime, call_loc)
 
-    def set_runtime(self, ct: Ciphertext, runtime: int, call_loc: Traceback) -> None:
-        self.where[ct] = (runtime, call_loc.filename, call_loc.positions) # type: ignore
-    
+    def set_runtime(self, ct: Ciphertext, runtime: int, call_loc: Frame) -> None:
+        while call_loc is not None:
+            (file_name, positions) = call_loc.location()
+            self.where[positions] = (runtime, file_name, positions)
+            call_loc = call_loc.caller()
