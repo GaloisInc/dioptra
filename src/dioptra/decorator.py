75073dcdbac6b3bfe408d6cc2d27c06eea80aaf1
James LaMar <james.lamar@galois.com>
2024-08-15 20:11:57 -0700

    Make `samples` an arg - not sure if this is correct

--- src/dioptra/decorator.py
+++ src/dioptra/decorator.py
@@ -11,10 +11,10 @@ from dioptra.analyzer.metrics.runtime import Runtime
 from dioptra.analyzer.utils.util import format_ns
 
 runtime_functions = []
-def dioptra_runtime(samples_file: str, limit: datetime.timedelta | None = None, description: str | None = None) -> Callable:  #TODO better type
+def dioptra_runtime(limit: datetime.timedelta | None = None, description: str | None = None) -> Callable:  #TODO better type
   def decorator(f):
     d = f.__name__ if description is None else description
-    runtime_functions.append((samples_file, limit, d, f))
+    runtime_functions.append((limit, d, f))
     return f
   
   return decorator
@@ -28,15 +28,16 @@ def timedelta_as_ns(d: datetime.timedelta) -> int:
 def annotate(file: str, function: str, outdir: str):
   pass
 
-def report_main(files: list[str]) -> None:
+def report_main(sample_file: str, files: list[str]) -> None:
+  samples = RuntimeSamples()
+  samples.read_json(sample_file)
+
   for file in files:
     print(f"loading {file}")
     runpy.run_path(file)
 
-  for (sample_file, limit, desc, f) in runtime_functions:
+  for (limit, desc, f) in runtime_functions:
     depth_analysis = MultDepth()
-    samples = RuntimeSamples()
-    samples.read_json(sample_file)
     runtime_analysis = Runtime(depth_analysis, samples)
     analyzer = Analyzer([runtime_analysis])
     
