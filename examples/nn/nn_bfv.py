416271c247824223a793ce5b7264fb6c19dc1ea9
Rawane Issa <rissa@galois.com>
2024-10-14 21:32:35 +0000

    Add examples for nn for every scheme

--- examples/nn/nn_bfv.py
+++ examples/nn/nn_bfv.py
@@ -165,77 +165,60 @@ class NN:
             layer_input = layer.train(cc, layer_input)
         return layer_input
 
-
-
 # make a cryptocontext and return the context and the parameters used to create it
-def setup_context() -> tuple[ofhe.CryptoContext, ofhe.CCParamsCKKSRNS]:
-    print("Setting up FHE program..")
-    parameters = ofhe.CCParamsCKKSRNS()
+def setup_context(mult_depth) -> tuple[
+    ofhe.CryptoContext,
+    ofhe.CCParamsBGVRNS,
+    ofhe.KeyPair,
+    list[ofhe.PKESchemeFeature],
+]:
+    # Sample Program: Step 1: Set CryptoContext
+    parameters = ofhe.CCParamsBFVRNS()
+    parameters.SetPlaintextModulus(65537)
+    parameters.SetMultiplicativeDepth(2)
 
-    secret_key_dist = ofhe.SecretKeyDist.UNIFORM_TERNARY
-    parameters.SetSecretKeyDist(secret_key_dist)
+    crypto_context = ofhe.GenCryptoContext(parameters)
+    # Enable features that you wish to use
+    features = [
+        ofhe.PKESchemeFeature.PKE,
+        ofhe.PKESchemeFeature.KEYSWITCH,
+        ofhe.PKESchemeFeature.LEVELEDSHE,
+    ]
+    for feature in features:
+        crypto_context.Enable(feature)
 
-    parameters.SetSecurityLevel(ofhe.SecurityLevel.HEStd_128_classic)
-    parameters.SetRingDim(1 << 17)
+    # Step 2: Key Generation
 
-    if ofhe.get_native_int() == 128:
-        rescale_tech = ofhe.ScalingTechnique.FIXEDAUTO
-        dcrt_bits = 78
-        first_mod = 89
-    else:
-        rescale_tech = ofhe.ScalingTechnique.FLEXIBLEAUTO
-        dcrt_bits = 59
-        first_mod = 60
+    # Generate a public/private key pair
+    key_pair = crypto_context.KeyGen()
 
-    parameters.SetScalingModSize(dcrt_bits)
-    parameters.SetScalingTechnique(rescale_tech)
-    parameters.SetFirstModSize(first_mod)
+    # Generate the relinearization key
+    crypto_context.EvalMultKeyGen(key_pair.secretKey)
 
-    level_budget = [4, 4]
+    # Generate the rotation evaluation keys
+    crypto_context.EvalRotateKeyGen(key_pair.secretKey, [1, 2, -1, -2])
 
-    levels_available_after_bootstrap = 10
-
-    depth = levels_available_after_bootstrap + ofhe.FHECKKSRNS.GetBootstrapDepth(
-        level_budget, secret_key_dist
-    )
-
-    parameters.SetMultiplicativeDepth(depth)
-
-    cryptocontext = ofhe.GenCryptoContext(parameters)
-    cryptocontext.Enable(ofhe.PKESchemeFeature.PKE)
-    cryptocontext.Enable(ofhe.PKESchemeFeature.KEYSWITCH)
-    cryptocontext.Enable(ofhe.PKESchemeFeature.LEVELEDSHE)
-    cryptocontext.Enable(ofhe.PKESchemeFeature.ADVANCEDSHE)
-    cryptocontext.Enable(ofhe.PKESchemeFeature.FHE)
-
-    cryptocontext.EvalBootstrapSetup(level_budget)
-
-    print("Setup complete..")
-    return (cryptocontext, parameters)
+    return (crypto_context, parameters, key_pair, features)
 
 
 # Actually run program and time it
 def main():
-    num_inputs = 11
-    (cc, parameters) = setup_context()
-
-    # do some additional setup for openfhe that is key dependent
-    key_pair = cc.KeyGen()
-    cc.EvalMultKeyGen(key_pair.secretKey)
-    cc.EvalBootstrapKeyGen(key_pair.secretKey, parameters.GetRingDim() >> 1)
+    num_inputs = 21
+    mult_depth = 21
+    (cc, parameters, key_pair, _) = setup_context(11)
 
     # encode and encrypt inputs labels
-    xs = [[random()] for i in range(num_inputs)]
-    xs_pt = [cc.MakeCKKSPackedPlaintext(x) for x in xs]
+    xs = [[i] for i in range(num_inputs)]
+    xs_pt = [cc.MakePackedPlaintext(x) for x in xs]
     for x in xs_pt:
         x.SetLength(1)
     xs_ct = [cc.Encrypt(key_pair.publicKey, x_pt) for x_pt in xs_pt]
 
     # Generate arbitrary nn 
-    neuron_weights = [0.1 for _ in range(11)]
-    layer_weights = [neuron_weights for _ in range(11)]
+    neuron_weights = [i for i in range(num_inputs)]
+    layer_weights = [neuron_weights for _ in range(num_inputs)]
     nn_weights = [layer_weights, layer_weights]
-    nn = NN.nn_from_plaintexts(cc, CKKS(), nn_weights)
+    nn = NN.nn_from_plaintexts(cc, BFV(), nn_weights)
 
     # time and run the program
     start_ns = time_ns()
@@ -246,7 +229,7 @@ def main():
     for r in results:
         result = cc.Decrypt(key_pair.secretKey, r)
         result.SetLength(1)
-        results_unpacked.append(result.GetCKKSPackedValue())
+        results_unpacked.append(result.GetPackedValue())
     print(results_unpacked)
 
     print(f"Actual runtime: {format_ns(end_ns - start_ns)}")
@@ -258,10 +241,10 @@ def report_runtime(cc: Analyzer):
     xs_ct = [cc.ArbitraryCT() for _ in range(num_inputs)]
 
     # Generate arbitrary nn 
-    neuron_weights = [0.1 for _ in range(11)]
-    layer_weights = [neuron_weights for _ in range(11)]
+    neuron_weights = [i for i in range(num_inputs)]
+    layer_weights = [neuron_weights for _ in range(num_inputs)]
     nn_weights = [layer_weights, layer_weights]
-    nn = NN.nn_from_plaintexts(cc, CKKS(), nn_weights)
+    nn = NN.nn_from_plaintexts(cc, BFV(), nn_weights)
 
     nn.train(cc, xs_ct)
 
