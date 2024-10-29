e9dc095384905b5fcd2fc54849f2a8608f4ffa84
James LaMar <james.lamar@galois.com>
2024-10-29 11:07:51 -0700

    Add comments to network examples

--- examples/network_binfhe.py
+++ examples/network_binfhe.py
@@ -1,3 +1,12 @@
+"""BinFHE Network Estimation Example
+
+This example shows a simple example of the dioptra's network estimation
+feature for a binfhe computation.  It simulates the sending of two
+ciphertexts over the network, after which an AND is performed.
+Then, the result of the AND is "sent" back - the send also being
+simulated.
+"""
+
 from dioptra.analyzer.binfhe.analyzer import BinFHEAnalyzer
 from dioptra.analyzer.utils.util import BPS
 from openfhe import BINGATE
@@ -7,6 +16,7 @@ from dioptra.decorator import dioptra_binfhe_runtime
 
 @dioptra_binfhe_runtime()
 def network_example(a: BinFHEAnalyzer):
+  # definition of the network parameters
   network = a.MakeNetwork(
     send_bps=BPS(Mbps=100),
     recv_bps=BPS(Gbps=1),
@@ -15,9 +25,13 @@ def network_example(a: BinFHEAnalyzer):
 
   ct1 = a.ArbitraryCT()
   ct2 = a.ArbitraryCT()
-  network.RecvCiphertext(ct1)
+
+  # simulate sending `ct1` and `ct2`
+  network.RecvCiphertext(ct1) 
   network.RecvCiphertext(ct2)
 
+  # do a computation
   ct3 = a.EvalBinGate(BINGATE.AND, ct1, ct2)
 
+  # simulate receiving the result
   network.SendCiphertext(ct3)
