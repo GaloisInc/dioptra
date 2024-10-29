1b4fb869b438e7720a107fcd5df40793e594265b
James LaMar <james.lamar@galois.com>
2024-10-29 11:07:51 -0700

    Add comments to network examples

--- examples/network_pke.py
+++ examples/network_pke.py
@@ -1,3 +1,12 @@
+"""PKE Network Estimation Example
+
+This example shows a simple example of the dioptra's network estimation
+feature for a binfhe computation.  It simulates the sending of two
+ciphertexts over the network, after which the product of the two is
+computed.  Then, the product is "sent" back - the send also being
+simulated.
+"""
+
 from dioptra.analyzer.pke.analysisbase import Analyzer
 from dioptra.analyzer.utils.util import BPS
 from dioptra.decorator import dioptra_runtime
@@ -5,17 +14,22 @@ from dioptra.decorator import dioptra_runtime
 
 @dioptra_runtime()
 def network_example(a: Analyzer):
+  # definition of the network parameters
   network = a.MakeNetwork(
     send_bps=BPS(Mbps=100),
     recv_bps=BPS(Gbps=1),
     latency_ms=50
   )
-
+  
   ct1 = a.ArbitraryCT(level=20)
   ct2 = a.ArbitraryCT()
+
+  # simulate sending `ct1` and `ct2`
   network.RecvCiphertext(ct1)
   network.RecvCiphertext(ct2)
-
+  
+  # compute the product
   ct3 = a.EvalMult(ct1, ct2)
 
+  # simulate receiving the result
   network.SendCiphertext(ct3)
