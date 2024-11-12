10fa32ddeca7e205e048d630c01fed75fe494275
Chris Phifer <cphifer@galois.com>
2024-11-11 19:43:14 -0800

    cli: Document module.
    And improve the documentation / help text of CLI commands and their
    parameters.

--- src/dioptra/cli.py
+++ src/dioptra/cli.py
@@ -1,3 +1,12 @@
+"""Dioptra CLI entrypoints.
+
+Definitions of the click-decorated functions that implement the Dioptra CLI.
+
+If you are a library author looking to build on / programmatically use the
+Dioptra CLI functionalities, see the dioptra.estimate and dioptra.context
+packages.
+"""
+
 from pathlib import Path
 
 import click
@@ -11,13 +20,23 @@ from dioptra.estimate.report import report_main
 
 @click.group()
 def cli() -> None:
-    """The Dioptra FHE platform."""
+    """Dioptra: Analysis of OpenFHE applications.
+
+    Dioptra is a library and command-line application for the analysis of Python
+    programs that use OpenFHE, providing tools for calibration (to produce
+    system-accurate estimates) and estimation (a "test runner" that looks for
+    specially-decorated functions in Python modules).
+    """
     pass
 
 
 @cli.group()
 def estimate() -> None:
-    """Estimate properties of an OpenFHE program."""
+    """Estimate runtime and memory performance of OpenFHE applications.
+
+    All of the dioptra estimate commands assume access to calibration data, and
+    modules containing appropriately-decorated estimation cases.
+    """
     pass
 
 
@@ -28,13 +47,13 @@ def estimate() -> None:
     "-cd",
     type=click.Path(exists=True),
     required=True,
-    help="File containing calibration data to use for the estimate.",
+    help="Calibration data file to use for estimates.",
 )
 def report(file: Path, calibration_data: Path) -> None:
-    """Estimate FHE runtime and memory usage for decorated functions.
+    """Report runtime and memory performance estimates for all estimation cases.
 
-    FILE is the python file to look for functions decorated with
-    "@dioptra_estimation" or "@dioptra_binfhe_estimation".
+    FILE is the Python file in which to look for estimation cases (functions
+    decorated with "@dioptra_estimation()" or "@dioptra_binfhe_estimation()").
     """
     report_main(str(calibration_data), [str(file)])
 
@@ -46,14 +65,14 @@ def report(file: Path, calibration_data: Path) -> None:
     "-cd",
     type=click.Path(exists=True),
     required=True,
-    help="File containing calibration data to use for the estimate.",
+    help="Calibration data file to use for estimates.",
 )
 @click.option(
     "--output",
     "-o",
     type=click.Path(file_okay=False, dir_okay=True, writable=True, path_type=Path),
     required=True,
-    help="Output directory for annotated files.",
+    help="Directory to which annotated files should be written.",
 )
 @click.option(
     "--name",
@@ -63,17 +82,23 @@ def report(file: Path, calibration_data: Path) -> None:
     help="Name of the estimation case to annotate.",
 )
 def annotate(file: Path, calibration_data: Path, name: str, output: Path) -> None:
-    """Annotate Python source files with estimated FHE runtimes.
+    """Annotate Python source files with estimated OpenFHE operation runtimes.
 
-    FILE is the python file to look for functions decorated with
-    "@dioptra_estimation" or "@dioptra_binfhe_estimation".
+    FILE is the Python file in which to look for estimation cases (functions
+    decorated with "@dioptra_estimation()" or "@dioptra_binfhe_estimation()").
     """
     annotate_main(str(calibration_data), str(file), name, str(output))
 
 
 @cli.group()
 def context() -> None:
-    """Generate FHE runtime/memory calibration data."""
+    """Generate runtime and memory calibration data for OpenFHE applications.
+
+    To not make assumptions about your system, Dioptra estimates performance
+    based on calibration data specific to your context. Dioptra's context
+    commands assume access to Python modules defining specific OpenFHE contexts,
+    which are particular selections of FHE schemes/parameters.
+    """
     pass
 
 
@@ -84,14 +109,14 @@ def context() -> None:
     "-n",
     type=str,
     required=True,
-    help="Name of the context function to generate data for.",
+    help="Name of the context function to use to generate calibration data.",
 )
 @click.option(
     "--output",
     "-o",
     type=click.Path(exists=False),
     required=True,
-    help="File to output calibration data to.",
+    help="File to which calibration data should be written.",
 )
 @click.option(
     "--sample-count",
@@ -101,10 +126,10 @@ def context() -> None:
     help="Number of samples to take (default: 5).",
 )
 def calibrate(file: Path, name: str, output: Path, sample_count: int):
-    """Run calibration for a decorated context function.
+    """Generate calibration data for a decorated context function.
 
-    FILE is the python file to look for functions decorated with
-    "@dioptra_context" or "@dioptra_binfhe_context".
+    FILE is the Python file in which to look for contexts (functions decorated
+    with "@dioptra_context()" or "@dioptra_binfhe_context()".)
     """
     calibrate_main([str(file)], name, str(output), sample_count)
 
@@ -112,10 +137,10 @@ def calibrate(file: Path, name: str, output: Path, sample_count: int):
 @context.command()
 @click.argument("file", type=click.Path(exists=True), required=True)
 def list(file: Path):
-    """List decorated context functions.
+    """List context functions.
 
-    FILE is the python file to look for functions decorated with
-    "@dioptra_context" or "@dioptra_binfhe_context".
+    FILE is the python file in which to look for contexts (functions decorated
+    with "@dioptra_context()" or "@dioptra_binfhe_context()".)
     """
     list_main([str(file)])
 
@@ -127,14 +152,18 @@ def list(file: Path):
     "-cd",
     type=click.Path(exists=True),
     required=True,
-    help="File containing calibration data to use for the estimate.",
+    help="Calibration data file to use for estimates.",
 )
 @click.option(
     "--output",
     "-o",
     type=click.Path(file_okay=False, dir_okay=True, writable=True, path_type=Path),
-    help="Output directory for render results.",
+    help="Directory to which the rendered website should be written.",
 )
 def render(file: Path, calibration_data: Path, output: Path) -> None:
-    """Run decorated functions and render analysis results."""
+    """Render a website for an estimation case.
+
+    FILE is the Python file in which to look for estimation cases (functions
+    decorated with "@diotpra_estimation()" or "@dioptra_binfhe_estimation()").
+    """
     render_main(str(calibration_data), str(file), str(output))
