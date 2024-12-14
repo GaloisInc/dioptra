3d1be3f150da603cf8d4e22dfacbb6a5dc5ab95b
James LaMar <james.lamar@galois.com>
2024-12-13 16:31:06 -0800

    rename stage to classify - add single perceptron example

--- benchmarks/src/dioptra_benchmarks/nn/ckks/estimates.py
+++ benchmarks/src/dioptra_benchmarks/nn/ckks/estimates.py
@@ -1,9 +1,28 @@
 from dioptra.estimate import dioptra_pke_estimation
 from dioptra.pke.analyzer import Analyzer
-from dioptra_examples.nn import NN
+from dioptra_examples.nn import NN, Neuron
 from dioptra_examples.schemes import CKKS
 
 import random
+
+
+def run_single_perceptron(cc: Analyzer, input_len: int):
+    weights = [cc.MakeCKKSPackedPlaintext([random.uniform(0, 1)]) for i in range(input_len)]
+    inputs = [cc.ArbitraryCT() for i in range(input_len)]
+    p = Neuron(cc, CKKS(), weights, None)
+    p.classify(cc, inputs)
+
+@dioptra_pke_estimation()
+def single_perceptron_classify_10(cc: Analyzer):
+    run_single_perceptron(cc, 10)
+
+@dioptra_pke_estimation()
+def single_perceptron_classify_100(cc: Analyzer):
+    run_single_perceptron(cc, 100)
+
+@dioptra_pke_estimation()
+def single_perceptron_classify_1000(cc: Analyzer):
+    run_single_perceptron(cc, 1000)
 
 @dioptra_pke_estimation()
 def nn_1_input_3_layer(cc: Analyzer):
@@ -17,4 +36,4 @@ def nn_1_input_3_layer(cc: Analyzer):
     nn_weights = [layer_weights for _ in range(num_layers)]
     nn = NN.nn_from_plaintexts(cc, CKKS(), nn_weights)
 
-    nn.train(cc, xs_ct)
+    nn.classify(cc, xs_ct)
