04b3522ded8263da5709d8385bac2349b893ef0e
Rawane Issa <rissa@galois.com>
2025-01-23 00:27:02 +0000

    dioptra/benchmarks/matrix/runner: Add CLI that is specific to this benchmark

--- benchmarks/src/dioptra_benchmarks/matrix/run_benchmarks.py
+++ benchmarks/src/dioptra_benchmarks/matrix/run_benchmarks.py
@@ -3,6 +3,7 @@ import pathlib
 import re
 import sys
 import subprocess
+import argparse
 
 import dioptra.utils.measurement
 
@@ -14,6 +15,7 @@ pke_estimates = loc.joinpath("pke_estimates.py").absolute()
 binfhe_estimates = loc.joinpath("binfhe_estimates.py").absolute()
 main_loc = loc.joinpath("main.py").absolute()
 
+context_choices = ["bfv_128", "bgv_128", "ckks_128", "binfhe_128"]
 
 def dioptra_estimate(ctx_name: str, est_loc: pathlib.Path):
   print(f"-- estimates for {ctx_name} --")
@@ -35,11 +37,11 @@ def rewrite_memory(blob: str) -> str:
     bytes = int(match.group(1)) * 1000
     fmtted = dioptra.utils.measurement.format_bytes(bytes)
     return mem_pattern.sub(f"Max memory used: {fmtted}", blob)
-  
-  return blob
-    
 
-def run_main(ctx_name: str, n: int):
+  return blob
+
+
+def dioptra_execute(ctx_name: str, n: int):
   print(f"Running main.py with context {ctx_name} and matrix size {n}x{n}")
   cmd = f"python {main_loc} --dim1 {n}x{n} --dim2 {n}x1 --context {ctx_name} --no-setup-runtime"
   cmd = with_mem_usage(cmd)
@@ -50,26 +52,54 @@ def run_main(ctx_name: str, n: int):
   print("-------------------------------")
   sys.stdout.flush()
 
-print("Estimates:")
+def run_all_benchmarks():
+  print("Estimates:")
 
-dioptra_estimate("bfv_128", pke_estimates)
-dioptra_estimate("bgv_128", pke_estimates)
-dioptra_estimate("ckks_128", pke_estimates)
-dioptra_estimate("binfhe_128", binfhe_estimates)
+  dioptra_estimate("bfv_128", pke_estimates)
+  dioptra_estimate("bgv_128", pke_estimates)
+  dioptra_estimate("ckks_128", pke_estimates)
+  dioptra_estimate("binfhe_128", binfhe_estimates)
 
-print()
-print()
+  print()
+  print()
 
-print("Actual Runtime:")
+  print("Actual Runtime:")
 
-for ctx in ["bfv_128", "bgv_128"]:
-  for dim in [4, 16, 64]:
-    run_main(ctx, dim)
+  for ctx in ["bfv_128", "bgv_128"]:
+    for dim in [4, 16, 64]:
+      dioptra_execute(ctx, dim)
 
-for ctx in ["ckks_128", "binfhe_128"]:
-  for dim in [4, 16]:
-    run_main(ctx, dim)
-    
-print()
-print()
+  for ctx in ["ckks_128", "binfhe_128"]:
+    for dim in [4, 16]:
+      dioptra_execute(ctx, dim)
 
+  print()
+  print()
+
+def main():
+  parser = argparse.ArgumentParser(
+                    prog='Dioptra Matrix x Vector Benchmarks',
+                    description='Benchmarks for Multiplication of degree-256 polynomial matrix X vector')
+
+  group_run = parser.add_mutually_exclusive_group(required=True)
+  group_run.add_argument('-exe','--execute', help='Run the benchmark in OpenFHE', action='store_true')
+  group_run.add_argument('-est','--estimate', help='Estimate the benchmark using Dioptra', action='store_true')
+  group_run.add_argument('-all','--runall', help='Run and estimate all the benchmarks', action='store_true')
+
+  parser.add_argument('-estloc','--estimateloc', help='The path of the functions to estimate', required='--estimate' in sys.argv, nargs='?', const=pke_estimates, type=str)
+  parser.add_argument('-d','--dimension', help='The dimension of the matrix and vector to benchmark', required='--execute' in sys.argv, type=int)
+  parser.add_argument('-cd','--ctxt', help='The context name to use for the benchmark', choices=context_choices, required=('--estimate' or '--execute') in sys.argv, type=str)
+
+  args = parser.parse_args()
+
+  if args.execute:
+    dioptra_execute(args.ctxt, args.dimension)
+  elif args.estimate:
+    dioptra_estimate(args.ctxt, args.estloc)
+  elif args.runall:
+    run_all_benchmarks()
+  else:
+    raise NotImplementedError
+
+if __name__ == '__main__':
+  main()
