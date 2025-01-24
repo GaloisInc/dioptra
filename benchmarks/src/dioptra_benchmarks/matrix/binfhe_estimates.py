ae299173679f2bbbe5a954e04545f26703b6145f
James LaMar <james.lamar@galois.com>
2025-01-24 12:15:41 -0800

    Add 8x8 to binfhe estimates

--- benchmarks/src/dioptra_benchmarks/matrix/binfhe_estimates.py
+++ benchmarks/src/dioptra_benchmarks/matrix/binfhe_estimates.py
@@ -13,6 +13,10 @@ def mk_arbitrary_matrix(cc: BinFHEAnalyzer, rows: int, cols: int):
 def mul4x4matrix_by_vector(cc: BinFHEAnalyzer):
   _ = mk_arbitrary_matrix(cc, 4, 4) * mk_arbitrary_matrix(cc, 4, 1)
 
+@dioptra_binfhe_estimation(description="multiply 8x8 matrix by a vector")
+def mul8x8matrix_by_vector(cc: BinFHEAnalyzer):
+  _ = mk_arbitrary_matrix(cc, 16, 16) * mk_arbitrary_matrix(cc, 16, 1)
+
 @dioptra_binfhe_estimation(description="multiply 16x16 matrix by a vector")
 def mul16x16matrix_by_vector(cc: BinFHEAnalyzer):
   _ = mk_arbitrary_matrix(cc, 16, 16) * mk_arbitrary_matrix(cc, 16, 1)
